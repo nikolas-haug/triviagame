da6ade3221dbaba4b0312dacd746bdd78ec2a6bf
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-04-09 16:57:07 -0500

    add inital timer functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,6 +28,30 @@ var questions = [newQuestion1, newQuestion2, newQuestion3, newQuestion4];
 //variable to keep track of question
 var questionCounter = 0;
 
+//variable for the timer
+var timeLeft = 30;
+
+//variable for the set timer interval
+var timer;
+
+//variable to track the status of the game
+var gameOver = false;
+
+//function to start the game
+function startTimer() {
+    var timer = setInterval(function() {
+        $('#timer').text(timeLeft);
+        timeLeft--;
+    }, 1000);
+};
+
+//function for the question timer
+// function gameTimer() {
+//     var timer = setInterval(function() {
+//         timeLeft--;
+//     }, 1000);
+// }
+
 //function to generate the questions and possible answers
 // TO DO randomize the selection of questions - put them into one object
 function makeQuestion() {
@@ -37,6 +61,7 @@ function makeQuestion() {
         newAnswers.append(questions[questionCounter].answers[i]);
         $('#answers').append(newAnswers);
     } 
+    startTimer();
     makeGuess(); 
 };
 
@@ -44,10 +69,22 @@ function makeGuess() {
     $('#answers').on('click', 'li', function() {
         var guess = ($(this).data('choice'));
         console.log(guess);
+        if(guess === questions[questionCounter].correctIndex) {
+            console.log("correct!");
+            clearInterval(timer);
+        } else {
+            console.log("wrong!");
+        }
     });
 };
 
+//function to stop/restart the timer
+function resetTimer() {
+    clearInterval(timer);
+};
+
 makeQuestion();
+// startGame();
 
 
 // $('#question').text(firstQuestion.question);
